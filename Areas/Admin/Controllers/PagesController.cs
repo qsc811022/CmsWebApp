216c2a4d46d1ae1506f64b149e5ea74e7e891f31
TedliuComputer\asus <qsc811022@gmail.com>
2023-01-27 23:04:25 +0800

    Add Page Function

--- Areas/Admin/Controllers/PagesController.cs
+++ Areas/Admin/Controllers/PagesController.cs
@@ -25,5 +25,67 @@ namespace CmsWebApp.Areas.Admin.Controllers
 
             return View(pageList);
         }
+
+        //GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Check model stats
+            if (! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db =new Db())
+            {
+                //Declare slug
+                string slug;
+
+                //Init pageDTO
+                PageDTO dto = new PageDTO();
+
+                //DTO title
+                dto.Title=model.Title;
+
+                //Check for and set slug if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //Make sure title and slug are unique
+                if (db.Pages.Any(x=>x.Title==model.Title) || db.Pages.Any(x=>x.Slug==slug))
+                {
+                    ModelState.AddModelError("","That title or slug already exist");
+                    return View(model);
+                }
+
+
+                //DTO the reset
+                dto.Slug=slug;
+                dto.Body=model.Body;
+                dto.HasSidebar=model.HasSidebar;
+                dto.Sorting=100;
+
+                //Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+            //Set TempData message
+            TempData["SM"]="You have added a new page!";
+
+            //Redirect
+            return RedirectToAction("AddPage");
+        }
+
     }
 }
