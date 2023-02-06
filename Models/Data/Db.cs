53f6832bb179bf8b1cc4acad6d090814164a0f86
TedliuComputer\asus <qsc811022@gmail.com>
2023-02-07 07:14:24 +0800

    add Sidebar

--- Models/Data/Db.cs
+++ Models/Data/Db.cs
@@ -9,6 +9,6 @@ namespace CmsWebApp.Models.Data
     public class Db:DbContext
     {
         public DbSet<PageDTO> Pages { get;set;}
-
+        public DbSet<SidebarDTO> Sidebar { get;set;}
     }
 }
