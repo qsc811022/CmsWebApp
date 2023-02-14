0940fcd6de73f5ff26391050d8765eb2480f835f
TedliuComputer\asus <qsc811022@gmail.com>
2023-02-14 21:30:28 +0800

    19.admin shop2

--- Areas/Admin/Controllers/ShopController.cs
+++ Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CmsWebApp.Models.Data;
+using CmsWebApp.Models.ViewModels.Shop;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,26 @@ namespace CmsWebApp.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
         {
-            return View();
+            //Declare a list of models
+            List<CategoryVM> categoryVMList;
+       
+            using (Db db = new Db())
+            {
+                //Init the list
+                categoryVMList = db.Categories
+                    .ToArray()
+                    .OrderBy(x=>x.Sorting)
+                    .Select(x=>new CategoryVM(x))
+                    .ToList();
+
+            }
+
+
+            //Return view with list
+            return View(categoryVMList);
         }
     }
 }
