0a198a439e3d8ba9862fe4089d4e72fdba7ec5e9
TedliuComputer\asus <qsc811022@gmail.com>
2023-02-19 21:40:17 +0800

    23

--- Areas/Admin/Controllers/ShopController.cs
+++ Areas/Admin/Controllers/ShopController.cs
@@ -32,5 +32,39 @@ namespace CmsWebApp.Areas.Admin.Controllers
             //Return view with list
             return View(categoryVMList);
         }
+        //Post:admin:/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            //Declare id
+            string id;
+
+            using (Db db =new Db())
+            {
+                //Check that the categroy name is unique
+                if(db.Categories.Any(x=>x.Name == catName))
+                {
+                    return "titletaken";
+                }
+                //Init DTO
+                CategoryDTO dto = new CategoryDTO();
+                //Add to DTO
+
+                dto.Name=catName;
+                dto.Slug=catName.Replace("","-").ToLower();
+                dto.Sorting=100;
+
+                //Save DTO
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                //Get the id
+                id = dto.Id.ToString();
+            }
+            //Return id
+
+
+            return id;
+        }
     }
 }
